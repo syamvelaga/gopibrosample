dfe7817072de0bc926a6ed6e08b6c6a4e8cd1c69
Bujji061219 <gopichandmedisetti@gmail.com>
2024-02-29 05:00:18 +0000

    just

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+
+// This is the list (static data) used in the application. You can move it to any component if needed.
+
+// Replace your code here
+const App = () => <h1>Hello World</h1>
+
+export default App
